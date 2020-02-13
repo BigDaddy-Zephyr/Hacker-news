e9383269b9e8cca8cb557792d7984f2ab3ccf8a8
bigdaddy-zephyr <anujduggal61@gmail.com>
2020-02-13 16:33:50 +0530

    fixed passport bugs

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -38,18 +38,18 @@ console.log("////////////////////////",req.password);
   console.log(data);
   var hashedpassword;
 
- bcrypt.hash(data.password, 10, function(err, hash) {
-          if (err) {
-            console.log("Error in hashing")
-          };
+ // bcrypt.hash(data.password, 10, function(err, hash) {
+ //          if (err) {
+ //            console.log("Error in hashing")
+ //          };
 
-          console.log("%%%%%%%%%%%%%%%%%%%%%%%%",hash);
-          // override the cleartext password with the hashed one
-       hashedpassword=hash;
-          // data.password=hashedpassword;
-  console.log("------", data.id, data.password);
+ //          console.log("%%%%%%%%%%%%%%%%%%%%%%%%",hash);
+ //          // override the cleartext password with the hashed one
+ //       hashedpassword=hash;
+ //          // data.password=hashedpassword;
+ //  console.log("------", data.id, data.password);
 
-  User.register(new User({ username: data.username, name:data.name }), data.password, function(
+  User.register(new User({ username: data.username }), data.password, function(
     err,
     user
   ) {
@@ -58,11 +58,11 @@ console.log("////////////////////////",req.password);
       return res.render("index", user);
     }
     console.log("!@!@!@!@@@@@@@@@@@@@@@",user);
-    console.log("lklklklkl", req.body.id, req.body.password);
+    // console.log("lklklklkl", req.body.id, req.body.password);
     res.redirect('/login');
   });
 
-      });
+      // });
 
  
 };
@@ -80,12 +80,20 @@ userController.login = function(req, res) {
 userController.doLogin = function(req, res) {
   passport.authenticate('local', function(err, user, info) {
     if (err) { console.log("KLKLKL",err); }
-    if (!user) { return res.redirect('/'); }
+    if (!user) { 
+      console.log("IIIIIIIIIIIIIII",info);
+      return res.redirect('/'); }
 
     // req / res held in closure
     req.logIn(user, function(err) {
-      if (err) { return 0; }
-      return res.send(user);
+      if (err) { console.log("}{}{}}{}{}{}{}{}",err);return 0; }
+
+      console.log("AAAAAAAAAAAA",user);
+
+
+      // return done(null, user);
+
+      return res.cookie("userid", user.username);;
     });
 
   })(req, res);
